94fb5f2f594b368588865b20b671a7c095fe6a66
ArturSpirenkov <arturspirenkov@gmail.com>
2023-10-02 17:58:56 +0300

    feat: router path

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Index = () => import('@/views/IndexView.vue')
+const Button = () => import('@/views/ButtonView.vue')
+const Typography = () => import('@/views/TypographyView.vue')
+const Checkbox = () => import('@/views/CheckboxView.vue')
+const Radiobutton = () => import('@/views/RadiobuttonView.vue')
+
+const history = createWebHistory()
+
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: Index,
+  },
+  {
+    path: '/button',
+    name: 'Button',
+    component: Button,
+  },
+  {
+    path: '/typography',
+    name: 'Typography',
+    component: Typography,
+  },
+  {
+    path: '/checkbox',
+    name: 'Checkbox',
+    component: Checkbox,
+  },
+  {
+    path: '/radiobutton',
+    name: 'Radiobutton',
+    component: Radiobutton,
+  },
+]
+
+const router = createRouter({
+  history,
+  routes,
+})
+
+export default router
